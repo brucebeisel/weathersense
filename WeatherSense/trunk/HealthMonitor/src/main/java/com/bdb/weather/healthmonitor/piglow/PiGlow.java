42d24bdce8411ddec71491206534924aed07f639
brucebeisel@gmail.com <brucebeisel@gmail.com@bfdf6112-06a0-c012-f6e4-b7d192253325>
2015-03-05 09:13:45 +0000

    More PiGlow updates.

--- WeatherSense/trunk/HealthMonitor/src/main/java/com/bdb/weather/healthmonitor/piglow/PiGlow.java
+++ WeatherSense/trunk/HealthMonitor/src/main/java/com/bdb/weather/healthmonitor/piglow/PiGlow.java
@@ -16,12 +16,88 @@
  */
 package com.bdb.weather.healthmonitor.piglow;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.pi4j.io.i2c.I2CBus;
+import com.pi4j.io.i2c.I2CDevice;
+import com.pi4j.io.i2c.I2CFactory;
+import com.pi4j.system.SystemInfo;
+
 /**
  *
  * @author Bruce
  */
 public class PiGlow {
+    private static final int ENABLE_OUTPUT_ADDR = 0x0;
+    private static final int FIRST_LED_ADDR = 0x1;
+    private static final int ENABLE_TOP_ARM_ADDR = 0x13;
+    private static final int ENABLE_LEFT_ARM_ADDR = 0x14;
+    private static final int ENABLE_RIGHT_ARM_ADDR = 0x15;
+    private static final int COMMIT_ADDR = 0x16;
+    private static final int I2C_ADDR = 0x54;
+    private static final byte VALUE = (byte)0xFF;
+    private static final byte ALL_OFF[] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
+    private I2CBus bus;
+    private I2CDevice device;
+    private final byte[] intensities = new byte[18];
 
     public PiGlow() {
+    }
+
+    public void initialize() throws IOException, InterruptedException {
+        SystemInfo.BoardType boardType = SystemInfo.getBoardType();
+        int busNumber;
+        switch (boardType) {
+            case ModelA_Rev0:
+            case ModelB_Rev1:
+            case ModelB_Rev2:
+                busNumber = I2CBus.BUS_0;
+                break;
+            default:
+                busNumber = I2CBus.BUS_1;
+                break;
+        }
+
+        bus = I2CFactory.getInstance(busNumber);
+        device = bus.getDevice(I2C_ADDR);
+
+        device.write(ENABLE_OUTPUT_ADDR, (byte)0x1);
+        device.write(ENABLE_TOP_ARM_ADDR, VALUE);
+        device.write(ENABLE_LEFT_ARM_ADDR, VALUE);
+        device.write(ENABLE_RIGHT_ARM_ADDR, VALUE);
+    }
+
+    public void commit() throws IOException {
+        device.write(COMMIT_ADDR, VALUE);
+    }
+
+    public void setLEDIntensity(PiGlowLED led, byte intensity) throws IOException {
+        led.setIntensity(intensity);
+        commit();
+    }
+
+    public void updateLEDs() throws IOException {
+        PiGlowLED.allLEDs().stream().forEach((led) -> {
+            intensities[led.getAddress() - FIRST_LED_ADDR] = (byte)led.getIntensity();
+        });
+
+        device.write(FIRST_LED_ADDR, intensities, 0, intensities.length);
+    }
+
+    public void allOff() throws IOException {
+        device.write(FIRST_LED_ADDR, ALL_OFF, 0, ALL_OFF.length);
+    }
+
+    public static final void main(String args[]) {
+        try {
+            PiGlow pg = new PiGlow();
+            pg.initialize();
+            pg.setLEDIntensity(PiGlowLED.findLed(PiGlowArm.TOP, PiGlowColor.RED), (byte)128);
+        }
+        catch (IOException | InterruptedException ex) {
+            Logger.getLogger(PiGlow.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
--- WeatherSense/trunk/HealthMonitor/src/main/java/com/bdb/weather/healthmonitor/piglow/PiGlowLED.java
+++ WeatherSense/trunk/HealthMonitor/src/main/java/com/bdb/weather/healthmonitor/piglow/PiGlowLED.java
@@ -16,7 +16,10 @@
  */
 package com.bdb.weather.healthmonitor.piglow;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -28,17 +31,49 @@ public class PiGlowLED {
     public int MAX_INTENSITY = 255;
     private final PiGlowArm arm;
     private final PiGlowColor color;
+    private final int address;
     private int intensity;
     private static final Map<Integer,PiGlowLED> leds = new HashMap<>();
+    private static final Map<PiGlowArm,List<PiGlowLED>> arms = new HashMap<>();
+    private static final Map<PiGlowColor,List<PiGlowLED>> colors = new HashMap<>();
+    private static final List<PiGlowLED> ledList = new ArrayList<>();
 
     static {
-        for (PiGlowArm arm : PiGlowArm.values()) {
-            for (PiGlowColor color : PiGlowColor.values()) {
-                int id = LedIdentifier(arm, color);
-                PiGlowLED led = new PiGlowLED(arm, color);
-                leds.put(id, led);
-            }
-        }
+        for (PiGlowArm arm : PiGlowArm.values())
+            arms.put(arm, new ArrayList<>());
+
+        for (PiGlowColor color : PiGlowColor.values())
+            colors.put(color, new ArrayList<>());
+
+        createLED(PiGlowArm.LEFT, PiGlowColor.WHITE, 13);
+        createLED(PiGlowArm.LEFT, PiGlowColor.BLUE, 15);
+        createLED(PiGlowArm.LEFT, PiGlowColor.GREEN, 4);
+        createLED(PiGlowArm.LEFT, PiGlowColor.YELLOW, 3);
+        createLED(PiGlowArm.LEFT, PiGlowColor.ORANGE, 2);
+        createLED(PiGlowArm.LEFT, PiGlowColor.RED, 1);
+
+        createLED(PiGlowArm.TOP, PiGlowColor.WHITE, 10);
+        createLED(PiGlowArm.TOP, PiGlowColor.BLUE, 5);
+        createLED(PiGlowArm.TOP, PiGlowColor.GREEN, 6);
+        createLED(PiGlowArm.TOP, PiGlowColor.YELLOW, 9);
+        createLED(PiGlowArm.TOP, PiGlowColor.ORANGE, 8);
+        createLED(PiGlowArm.TOP, PiGlowColor.RED, 7);
+
+        createLED(PiGlowArm.RIGHT, PiGlowColor.WHITE, 11);
+        createLED(PiGlowArm.RIGHT, PiGlowColor.BLUE, 12);
+        createLED(PiGlowArm.RIGHT, PiGlowColor.GREEN, 14);
+        createLED(PiGlowArm.RIGHT, PiGlowColor.YELLOW, 16);
+        createLED(PiGlowArm.RIGHT, PiGlowColor.ORANGE, 17);
+        createLED(PiGlowArm.RIGHT, PiGlowColor.RED, 18);
+    }
+
+    private static void createLED(PiGlowArm arm, PiGlowColor color, int address) {
+        PiGlowLED led = new PiGlowLED(arm, color, address);
+        leds.put(led.getIdentifier(), led);
+        arms.get(led.getArm()).add(led);
+        colors.get(led.getColor()).add(led);
+        ledList.add(led);
+
     }
 
     public static PiGlowLED findLed(PiGlowArm arm, PiGlowColor color) {
@@ -46,13 +81,26 @@ public class PiGlowLED {
         return leds.get(id);
     }
 
+    public static List<PiGlowLED> armLEDs(PiGlowArm arm) {
+        return Collections.unmodifiableList(arms.get(arm));
+    }
+
+    public static List<PiGlowLED> colorLEDs(PiGlowColor color) {
+        return Collections.unmodifiableList(colors.get(color));
+    }
+
+    public static List<PiGlowLED> allLEDs() {
+        return Collections.unmodifiableList(ledList);
+    }
+
     private static int LedIdentifier(PiGlowArm arm, PiGlowColor color) {
         return arm.ordinal() << 8 | color.ordinal();
     }
 
-    private PiGlowLED(PiGlowArm arm, PiGlowColor color) {
+    private PiGlowLED(PiGlowArm arm, PiGlowColor color, int address) {
         this.arm = arm;
         this.color = color;
+        this.address = address;
     }
 
     public void setIntensity(int value) {
@@ -67,7 +115,15 @@ public class PiGlowLED {
         return color;
     }
 
+    public int getAddress() {
+        return address;
+    }
+
     public int getIntensity() {
         return intensity;
+    }
+
+    public int getIdentifier() {
+        return LedIdentifier(arm, color);
     }
 }
