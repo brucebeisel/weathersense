d7c9a2ae88f990da4f64c8b693e4b92c39d743f5
Bruce Beisel <brucebeisel@gmail.com>
2015-11-09 23:49:39 -0800

    Beginning of JavaFX version

--- WeatherSenseDisplayJavaFX/src/main/java/com/bdb/weather/javafx/WeatherSenseDisplay.java
+++ WeatherSenseDisplayJavaFX/src/main/java/com/bdb/weather/javafx/WeatherSenseDisplay.java
@@ -8,7 +8,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
-public class MainApp extends Application {
+public class WeatherSenseDisplay extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
