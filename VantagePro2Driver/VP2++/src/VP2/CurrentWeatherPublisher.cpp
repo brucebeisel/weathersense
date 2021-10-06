4d86a2d4d85cabd222e54c6c76443ddab54513d9
Bruce Beisel <brucebeisel@gmail.com>
2021-10-06 18:51:38 -0400

    Fixed issues with initialized Vantage Pro2 console, UV index and added UDP publisher for current weather

---
+++ VantagePro2Driver/VP2++/src/VP2/CurrentWeatherPublisher.cpp
@@ -0,0 +1,145 @@
+/* 
+ * Copyright (C) 2021 Bruce Beisel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <ifaddrs.h>
+#include <string.h>
+#include <string>
+#include "VP2Logger.h"
+#include "CurrentWeatherPublisher.h"
+#include "CurrentWeather.h"
+
+using namespace std;
+
+namespace vp2 {
+const std::string CurrentWeatherPublisher::MULTICAST_HOST = "224.0.0.120";
+
+CurrentWeatherPublisher::CurrentWeatherPublisher() : logger(VP2Logger::getLogger("CurrentWeatherPublisher"))
+{
+    createSocket();
+}
+
+CurrentWeatherPublisher::~CurrentWeatherPublisher()
+{
+    if (socketId != NO_SOCKET)
+	close(socketId);
+}
+
+void
+CurrentWeatherPublisher::sendCurrentWeather(const CurrentWeather & cw)
+{
+    if (socketId == NO_SOCKET)
+        return;
+
+    std::string s = cw.formatMessage();
+    const char * data = s.c_str();
+    size_t length = strlen(data);
+    if (sendto(socketId, data, length, 0, (struct sockaddr *)&groupAddr, sizeof(groupAddr)) != length) {
+	int e = errno;
+	logger.log(VP2Logger::VP2_WARNING) <<  "sendto() for current weather failed. Errno = " << e << endl;
+    }
+    else
+        logger.log(VP2Logger::VP2_INFO) << "Published current weather: " << data << endl;
+}
+
+bool
+CurrentWeatherPublisher::getLocalIpAddress(struct sockaddr_in & saddr)
+{
+    bool rv = false;
+    struct ifaddrs *addrs;
+    struct ifaddrs *tmp;
+
+    getifaddrs(&addrs);
+    tmp = addrs;
+
+    while (tmp) {
+        if (tmp->ifa_addr && tmp->ifa_addr->sa_family == AF_INET) {
+            struct sockaddr_in *pAddr = (struct sockaddr_in *)tmp->ifa_addr;
+            if (strncmp(tmp->ifa_name, "lo", 2) != 0) {
+                printf("%s: %s\n", tmp->ifa_name, inet_ntoa(pAddr->sin_addr));
+                saddr = *pAddr;
+                rv = true;
+                break;
+            }
+                
+        }
+
+        tmp = tmp->ifa_next;
+    }
+
+    freeifaddrs(addrs);
+
+    return rv;
+}
+
+bool
+CurrentWeatherPublisher::createSocket()
+{
+    if ((socketId = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        socketId = NO_SOCKET;
+        return false;
+    }
+
+    memset((char *)&groupAddr, 0, sizeof(groupAddr));
+    groupAddr.sin_family = AF_INET;
+    groupAddr.sin_addr.s_addr = inet_addr(MULTICAST_HOST.c_str());
+    groupAddr.sin_port = htons(MULTICAST_PORT);
+
+    char loopback = 0;
+    if (setsockopt(socketId, IPPROTO_IP, IP_MULTICAST_LOOP, &loopback, sizeof(loopback)) < 0) {
+	int e = errno;
+	logger.log(VP2Logger::VP2_ERROR) <<  "setsockopt() for loopback failed. Errno = " << e << endl;
+        close(socketId);
+        socketId = NO_SOCKET;
+        return false;
+    }
+
+    struct sockaddr_in saddr;
+    if (!getLocalIpAddress(saddr)) {
+	logger.log(VP2Logger::VP2_ERROR) <<  "setsockopt() getting local IP address failed." << endl;
+        close(socketId);
+        socketId = NO_SOCKET;
+        return false;
+    }
+
+    //struct in_addr local;
+    //local.s_addr = inet_addr("9.5.1.1");
+    if (setsockopt(socketId, IPPROTO_IP, IP_MULTICAST_IF, (char *)&saddr.sin_addr, sizeof(saddr.sin_addr)) < 0) {
+	int e = errno;
+	logger.log(VP2Logger::VP2_ERROR) <<  "setsockopt() for local interface failed. Errno = " << e << endl;
+        close(socketId);
+        socketId = NO_SOCKET;
+        return false;
+    }
+
+    unsigned char ttl = 2;
+    if (setsockopt(socketId, IPPROTO_IP, IP_MULTICAST_TTL, (char *)&ttl, sizeof(ttl)) < 0) {
+	int e = errno;
+	logger.log(VP2Logger::VP2_ERROR) <<  "setsockopt() for TTL failed. Errno = " << e << endl;
+        close(socketId);
+        socketId = NO_SOCKET;
+        return false;
+    }
+
+    logger.log(VP2Logger::VP2_INFO) << "Multicast socket created successfully" << endl;
+    return true;
+}
+
+}
